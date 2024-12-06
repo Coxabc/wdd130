a5bd6b6a88f50dcd2aa6a97e817d823cfc7cd37c
Coxabc <cox24018@byui.edu>
2024-12-05 18:57:29 -0700

    added styling to subpage 1

--- hitlist/script.js
+++ hitlist/script.js
@@ -58,6 +58,24 @@ function showSlide(index) {
 // Initial setup
 showSlide(currentSlide);
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
 
 
 
